f0bfeac901f3cc23b7439f4fc68d760e6061563f
Matthew White <mwhite1998@gmail.com>
2018-06-04 11:14:18 -0700

    Used more aggressive autopep8

--- StyleAutoPEP8.py
+++ StyleAutoPEP8.py
@@ -1,16 +1,14 @@
-x = {'a': 37, 'b': 42,
+x = {'a': 37, 'b': 42, 'c': 927}
 
-     'c': 927}
-
-y = 'hello ''world'
-z = 'hello '+'world'
+y = 'hello ' 'world'
+z = 'hello ' + 'world'
 a = 'hello {}'.format('world')
 
 
-class StyleCheck   (object):
+class StyleCheck(object):
     @staticmethod
     def f(foo):
-        return 37 * -2*foo
+        return 37 * -2 * foo
 
     def g(self, b, c=42):
         return {b, c}
@@ -19,4 +17,4 @@ class StyleCheck   (object):
 def f(val):
     foo = 3
     bar = 4
-    return 37 - val[42-foo:    bar**3]
+    return 37 - val[42 - foo:bar**3]
