9c23391578cc304f15b5066ad7a2db5092b349e1
Matthew White <mwhite1998@gmail.com>
2018-06-04 10:09:52 -0700

    Added PyCharm style

--- StylePyCharm.py
+++ StylePyCharm.py
@@ -1,18 +1,22 @@
-x = {'a':37, 'b':42,
+x = {'a': 37, 'b': 42,
 
-'c':927}
+     'c': 927}
 
-y='hello ''world'
-z='hello '+'world'
-a='hello {}'.format('world')
-class StyleCheck   (   object    ):
+y = 'hello ''world'
+z = 'hello ' + 'world'
+a = 'hello {}'.format('world')
+
+
+class StyleCheck(object):
     @staticmethod
+    def f(foo):
+        return 37 * -2 * foo
 
-    def f   (foo  ):
-        return 37 * -2*foo
     def g(self, b, c=42):
-        return   { b,c}
-def f  (   val ):
-    foo=3
-    bar=4
-    return        37 - val[42-foo  :    bar**3]
+        return {b, c}
+
+
+def f(val):
+    foo = 3
+    bar = 4
+    return 37 - val[42 - foo:    bar ** 3]
