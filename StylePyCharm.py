4fe93269e05bdc604f4194d8717bf59947afaa84
Matthew White <mwhite1998@gmail.com>
2018-06-04 10:00:57 -0700

    Add file for PyCharm specific formatter

---
+++ StylePyCharm.py
@@ -0,0 +1,19 @@
+x = {'a':37, 'b':42,
+
+'c':927}
+
+y='hello ''world'
+z='hello '+'world'
+a='hello {}'.format('world')
+class StyleCheck   (   object    ):
+    @staticmethod
+
+    def f   (foo  ):
+        return 37 * -2*foo
+    @staticmethod
+    def g( b, c=42):
+        return   { b,c}
+def f  (   val ):
+    foo=3
+    bar=4
+    return        37 - val[42-foo  :    bar**3]
