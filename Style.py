57dd3d1e7c106b759b902eefc00e1f65688f06a8
Matthew White <mwhite1998@gmail.com>
2018-06-04 09:41:01 -0700

    Add style files with duplicate

---
+++ Style.py
@@ -0,0 +1,19 @@
+x = {'a':37, 'b':42,
+
+'c':927}
+
+y='hello ''world'
+z='hello '+'world'
+a='hello {}'.format('world')
+class StyleCheck   (   object    ):
+    @staticmethod
+
+    def f   (self  ):
+        return 37 * -2
+    @staticmethod
+    def g( b, c=42):
+        return   { b,c}
+def f  (   val ):
+    foo=3
+    bar=4
+    return        37 - val[42 - foo:bar ** 3]
